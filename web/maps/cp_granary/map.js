3b57ba6d51a3e21e632cf65ebaf9f9f02f5c4332
Brian Barnekow <brian.barnekow@gmail.com>
2011-01-19 20:04:51 -0600

    Got granary canvas to work.

--- web/maps/cp_granary/map.js
+++ web/maps/cp_granary/map.js
@@ -1,25 +1,27 @@
 /////////////////////////////////////////////////////////////////////////////////////
 // GranaryMap class - Abstract class to perform operations onto the canvas for the specific map.
 /////////////////////////////////////////////////////////////////////////////////////
-function GranaryMap() {
-	this.minX = -6255;
-	this.maxX = 6264;
-	this.minY = -2907;
-	this.maxY = -192;
-	this.imgWidth = 1024;
-	this.imgHeight = 228;
-	this.flipXY = true;
-	this.negX = true;
-	this.capturePointRadius = 10;
-	this.mapImageLocation = './granary.jpg';
-	this.capturePoints = [
-		new CapturePoint("#Badlands_cap_cp3", new Coordinate(-1532,0), "") //midpt, neutral
-		,new CapturePoint("#Badlands_cap_blue_cp1", new Coordinate(-1601, 5184), "blue") //still slightly off.
-		,new CapturePoint("#Badlands_cap_blue_cp2", new Coordinate(-1537, 2963), "blue")
-		,new CapturePoint("#Badlands_cap_red_cp1", new Coordinate(-1472, -5185), "red")
-		,new CapturePoint("#Badlands_cap_red_cp2", new Coordinate(-1535, -2949), "red")
-	];
-}
-GranaryMap.prototype = new GameMap();
-GranaryMap.prototype.constructor = GranaryMap;
+var GranaryMap = GameMap.extend({
+  init: function(){
+    this._super();
+    
+	  this.minX = -6255;
+	  this.maxX = 6264;
+	  this.minY = -2907;
+	  this.maxY = -192;
+	  this.imgWidth = 1024;
+	  this.imgHeight = 228;
+	  this.flipXY = true;
+	  this.negX = true;
+	  this.mapImageLocation = '/maps/cp_granary/map.jpg';
+	  this.capturePoints = [
+	    //first value should be the log's name for the point, the second value should be the value that shows up in game
+		  new CapturePointDrawable("Granary_cap_cp3", "Central Control Point", this.generateImageCoordinate(new Coordinate(-1532,0)), "", this.capturePointRadius) //midpt, neutral
+		  ,new CapturePointDrawable("Granary_cap_blue_cp1", "BLU Base", this.generateImageCoordinate(new Coordinate(-1601, 5184)), "blue", this.capturePointRadius) //still slightly off.
+		  ,new CapturePointDrawable("Granary_cap_blue_cp2", "BLU Warehouse", this.generateImageCoordinate(new Coordinate(-1537, 2963)), "blue", this.capturePointRadius)
+		  ,new CapturePointDrawable("Granary_cap_red_cp1", "RED Base", this.generateImageCoordinate(new Coordinate(-1472, -5185)), "red", this.capturePointRadius)
+		  ,new CapturePointDrawable("Granary_cap_red_cp2", "RED Warehouse", this.generateImageCoordinate(new Coordinate(-1535, -2949)), "red", this.capturePointRadius)
+	  ];
+	}
+});
 gameMapObj = new GranaryMap(); //set the global map object to use our new GranaryMap object.
