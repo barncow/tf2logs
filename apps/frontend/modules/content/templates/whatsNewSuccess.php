04f5fc74c5540fa4dedd8af2ca9a2fc3cf95078f
Brian Barnekow <brian.barnekow@gmail.com>
2011-03-09 19:17:37 -0600

    Updated whats new page for the new bugfix

--- apps/frontend/modules/content/templates/whatsNewSuccess.php
+++ apps/frontend/modules/content/templates/whatsNewSuccess.php
@@ -2,11 +2,23 @@
 $sf_response->setTitle("What's New - TF2Logs.com");
 use_helper('PageElements');
 ?>
+
 <div id="whatsNewContainer">
+
+<?php
+$s = <<<EOD
+<h3>Disconnected Due to No Steam Login - Throwing Error</h3>
+<p>It appears that a log line for a user that disconnects due to a "No Steam logon" has a line break which causes a fragment of the line to go to another line. The parser would then cause an error on this line because it is invalid. The parser will now ignore this corrupted line.</p>
+EOD;
+echo outputInfoBox("entry1", "Updates for March 9, 2010", $s);
+?>
+
+<br/>
+
 <?php
 $s = <<<EOD
 <p>So, I whipped this page up real quick to let you all know what changes I have been making. This page is somewhat a work in progress. Onward with the changes:</p>
-<h3>Fixed issue with Dead Ringer deaths being counted</h3>
+<h3>Fixed issue with Dead Ringer Deaths being Counted</h3>
 <p>Before, Dead Ringer spies that "die" were being tracked for kills, deaths and assists, which in some cases would significantly inflate the numbers. All existing logs have been re-generated to update the numbers from this change.</p>
 <h3>Logs with Illegal Chars</h3>
 <p>There was a small issue where some character's in a player's name could not be handled properly (not just in the log parser itself, but some text editors even had a hard time with them). These characters have been replaced with the '?' character.</p>
@@ -17,4 +29,5 @@ $s = <<<EOD
 EOD;
 echo outputInfoBox("entry1", "Updates for March 8, 2010", $s);
 ?>
+
 </div>
