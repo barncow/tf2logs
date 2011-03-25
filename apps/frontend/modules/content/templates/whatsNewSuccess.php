5cc2918e513d8f5af5a8ad7420d8eeb476b1c449
Brian Barnekow <brian.barnekow@gmail.com>
2011-03-25 14:42:21 -0500

    updated whatsnew

--- apps/frontend/modules/content/templates/whatsNewSuccess.php
+++ apps/frontend/modules/content/templates/whatsNewSuccess.php
@@ -1,18 +1,32 @@
 <?php 
 $sf_response->setTitle("What's New - TF2Logs.com");
 use_helper('PageElements');
+
+$statpagehref = url_for('@plugins').'#suppstats';
 ?>
 
 <div id="whatsNewContainer">
 
 <?php
+$annuitLink = link_to('Annuit Coeptis', '@player_by_numeric_steamid?id=76561197983800058');
+$s = <<<EOD
+<h3>Fixed Some Issues with Corrupt Lines and Map Recognition</h3>
+<p>There seem to be more cases where line breaks are being erroneously entered into the log than expected. When coming upon one of these lines, the site would just reject the whole log file. Now, only the offending line will be ignored.</p>
+<p>There is also a fix to a somewhat hidden feature - automatic map recognition. TF2 will log what map the current log file is for, but generally too soon for it to show up in the log that gets saved. This will be something that will be added to the <a href="$statpagehref">Supplemental Stats Plugin</a>. But, sometimes it will get saved to the log. Originally, the site would not recognize this line outside of the tournament mode - now it does.</p>
+<p>Thanks to $annuitLink for bringing these issues to my attention.</p>
+EOD;
+echo outputInfoBox("entry7", "Updates for March 25, 2010", $s);
+?>
+
+<br/>
+
+<?php
 $log314Link = link_to('Here is a sample log with these new features added.', '@log_by_id?id=314');
-$statpagehref = url_for('@plugins').'#suppstats';
 $s = <<<EOD
 <h3>Multiple Features Added, Bugs Removed</h3>
 <p>There were some small, minor bugfixes, and you will probably not notice them. There have also been some updates to the <a href="$statpagehref">Supplemental Stats Plugin</a> by Cinq and Annuit Coeptis, which has just been released! These updates include tracking game pauses within the log, tracking how much healing is done per player, and what items, such as medkits, were picked up. Because pause logging is included, each log now has Playable Time calculated, which is the total time for the game, without time for pauses (if they are in the log) and without time between halves. This allows for the calculation of Per Minute stats, along with their Per Death counterparts.</p> <p>$log314Link</p>
 EOD;
-echo outputInfoBox("entry4", "Updates for March 20, 2010", $s);
+echo outputInfoBox("entry6", "Updates for March 20, 2010", $s);
 ?>
 
 <br/>
@@ -22,7 +36,7 @@ $s = <<<EOD
 <h3>Added Support for New Maps</h3>
 <p>I have added Log Viewer support for cp_steel, pl_badwater, and cp_dustbowl.</p>
 EOD;
-echo outputInfoBox("entry4", "Updates for March 15, 2010", $s);
+echo outputInfoBox("entry5", "Updates for March 15, 2010", $s);
 ?>
 
 <br/>
@@ -32,7 +46,7 @@ $s = <<<EOD
 <h3>Added Support for Cinq's Damage Plugin</h3>
 <p>Cinq has made a <a href="$statpagehref">damage plugin</a>. There will be some revisions of this, but TF2Logs.com will maintain support for this feature.</p>
 EOD;
-echo outputInfoBox("entry3", "Updates for March 13, 2010", $s);
+echo outputInfoBox("entry4", "Updates for March 13, 2010", $s);
 ?>
 
 <br/>
@@ -42,7 +56,7 @@ $s = <<<EOD
 <h3>Mirroring on Certain Maps</h3>
 <p>Some map images, like pl_goldrush, cp_gravelpit, and pl_upward were mirrored making them hard to read. This is now fixed. (Hit refresh in your browser if you do not see a change.)</p>
 EOD;
-echo outputInfoBox("entry2", "Updates for March 10, 2010", $s);
+echo outputInfoBox("entry3", "Updates for March 10, 2010", $s);
 ?>
 
 <br/>
@@ -53,7 +67,7 @@ $s = <<<EOD
 <h3>Disconnected Due to No Steam Login - Throwing Error</h3>
 <p>It appears that a log line for a user that disconnects due to a "No Steam logon" has a line break which causes a fragment of the line to go to another line. The parser would then cause an error on this line because it is invalid. The parser will now ignore this corrupted line. (Thanks to $bizLink for helping me fix this issue)</p>
 EOD;
-echo outputInfoBox("entry1", "Updates for March 9, 2010", $s);
+echo outputInfoBox("entry2", "Updates for March 9, 2010", $s);
 ?>
 
 <br/>
