a81dc16305630965261605840bc2e4a32acc6070
Brian Barnekow <brian.barnekow@gmail.com>
2011-03-26 15:07:15 -0500

    fixed issue with page numbers in search results not outputting correctly.

--- apps/frontend/lib/helper/SearchHelper.php
+++ apps/frontend/lib/helper/SearchHelper.php
@@ -6,7 +6,7 @@ value is replaced with pagenum. If it is not found, it is added.
 */
 function replacePageParam($url, $pageNum, $pageParam = "page") {
   $matches;
-  $url = preg_replace("/".$pageParam."=[\d*?]/", $pageParam."=".$pageNum, $url, -1, $matches);
+  $url = preg_replace("/".$pageParam."=\d+/", $pageParam."=".$pageNum, $url, -1, $matches);
   if($matches === 0) {
     $sep = "&";
     if(strpos($url, "?") === false) $sep = "?";
