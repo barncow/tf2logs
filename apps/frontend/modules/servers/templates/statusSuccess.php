d353c31a9354771511742a1aa17b5d00e1850fc8
Brian Barnekow <brian.barnekow@gmail.com>
2011-04-03 13:42:07 -0500

    some corrections to status

--- apps/frontend/modules/servers/templates/statusSuccess.php
+++ apps/frontend/modules/servers/templates/statusSuccess.php
@@ -13,7 +13,14 @@ Last Message Received: $lastmessage<br/>
 EOD;
 
 if($server->getStatus() == Server::STATUS_NOT_VERIFIED && checkAccess($sf_user, $server->getServerGroup()->getOwnerPlayerId())) {
-  $s .= '<p>This server has not yet been verified. Please follow the instructions on '.link_to('this page', '@server_verify_single?slug='.$server->getSlug()).' to find out how to get this server verified and active. If you have already followed those instructions, try refreshing this page in a few moments.</p><p>Otherwise, check the instructions and make sure that the server address in your configuration for the logaddress_add line is correct, and that you have entered the proper verification code.</p>';
+  $verifylink;
+  if($server->getServerGroup()->getGroupType() == ServerGroup::GROUP_TYPE_SINGLE_SERVER) {
+    $verifylink = link_to('this page', '@server_verify_single?slug='.$server->getSlug());
+  } else if($server->getServerGroup()->getGroupType() == ServerGroup::GROUP_TYPE_MULTI_SERVER) {
+    $verifylink = link_to('this page', '@server_verify_group?slug='.$server->getServerGroup()->getSlug().'&server_slug='.$server->getSlug());
+  }
+
+  $s .= '<p>This server has not yet been verified. Please follow the instructions on '.$verifylink.' to find out how to get this server verified and active. If you have already followed those instructions, try refreshing this page in a few moments.</p><p>Otherwise, check the instructions and make sure that the server address in your configuration for the logaddress_add line is correct, and that you have entered the proper verification code.</p>';
 }
 
 echo '<div id="contentContainer">';
