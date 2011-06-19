ce0a49055e4000171410cd07376d7d99103f0dc7
Brian Barnekow <brian.barnekow@gmail.com>
2011-06-19 09:06:04 -0500

    trying a quick fix for prod

--- lib/form/doctrine/LogForm.class.php
+++ lib/form/doctrine/LogForm.class.php
@@ -1,6 +1,32 @@
 <?php
 
 /**
+Extension of sfValidatorString to clean up whitespace and ASCII control chars.
+*/
+class CleanStringValidatorTemp extends sfValidatorString {
+  public function __construct($options = array(), $messages = array()) {
+
+    $this->addOption('throw_global_error', false);
+
+    parent::__construct($options, $messages);
+  }
+  
+  protected function configure($options = array(), $messages = array()) {
+    parent::configure($options, $messages);
+  }
+  
+  protected function doClean($value) {
+    
+    $clean = (string) $value;
+    $clean = preg_replace('/[\x00-\x1F\x7F]/', '', $clean);
+    $clean = trim($clean);
+
+    return parent::doClean($clean);
+  }
+}
+
+
+/**
  * Form to upload a log.
  *
  * @package    tf2logs
@@ -17,8 +43,8 @@ class LogForm extends sfForm
     ));
     
     $this->setValidators(array(
-      'name'       => new CleanStringValidator(array('max_length' => 100, 'required'   => false)),
-      'map_name'   => new CleanStringValidator(array('max_length' => 50, 'required' => false)),
+      'name'       => new CleanStringValidatorTemp(array('max_length' => 100, 'required'   => false)),
+      'map_name'   => new CleanStringValidatorTemp(array('max_length' => 50, 'required' => false)),
       'logfile'    => new sfValidatorFile(array(
                         'required'   => true,
                         'path'       => sfConfig::get('sf_upload_dir').'/logs',
