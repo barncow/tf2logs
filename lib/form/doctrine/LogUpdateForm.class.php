9848fbbe0fbdcaf659a8d77797a8ed4e55f1803e
Brian Barnekow <brian.barnekow@gmail.com>
2011-06-19 09:07:58 -0500

    trying a quick fix for prod

--- lib/form/doctrine/LogUpdateForm.class.php
+++ lib/form/doctrine/LogUpdateForm.class.php
@@ -1,6 +1,31 @@
 <?php
 
 /**
+Extension of sfValidatorString to clean up whitespace and ASCII control chars.
+*/
+class CleanStringValidatorTempp extends sfValidatorString {
+  public function __construct($options = array(), $messages = array()) {
+
+    $this->addOption('throw_global_error', false);
+
+    parent::__construct($options, $messages);
+  }
+  
+  protected function configure($options = array(), $messages = array()) {
+    parent::configure($options, $messages);
+  }
+  
+  protected function doClean($value) {
+    
+    $clean = (string) $value;
+    $clean = preg_replace('/[\x00-\x1F\x7F]/', '', $clean);
+    $clean = trim($clean);
+
+    return parent::doClean($clean);
+  }
+}
+
+/**
  * Form to update a log.
  *
  * @package    tf2logs
@@ -29,8 +54,8 @@ class LogUpdateForm extends BaseLogForm
     unset($this->widgetSchema['updated_at']);
     unset($this->validatorSchema['updated_at']);
     
-    $this->validatorSchema['name'] = new CleanStringValidator(array('min_length' => 1, 'max_length' => 100));
-    $this->validatorSchema['map_name'] = new CleanStringValidator(array('max_length' => 50, 'required' => false));
+    $this->validatorSchema['name'] = new CleanStringValidatorTempp(array('min_length' => 1, 'max_length' => 100));
+    $this->validatorSchema['map_name'] = new CleanStringValidatorTempp(array('max_length' => 50, 'required' => false));
   }
   
   
