c36a91d06a1d6e7c9285034b4f2ae10647d00efb
Brian Barnekow <brian.barnekow@gmail.com>
2011-07-16 13:31:50 -0500

    generified the dynamic module loading function.

--- app.js
+++ app.js
@@ -55,18 +55,28 @@ require('./lib/schemas.js')(mongoose); //pull in models
 /**
   Add our routes. Breaking these out into separate files to keep things clean.
 */
-(function(app, conf, mongoose) {
-  //set our base directory for routes
-  var baseDir = __dirname + '/routes';
-
-  //read the contents. Using sync here because we need to get results before moving on with server setup.
-  var files = require('fs').readdirSync(baseDir);
-  for(var i in files) {
-    //if the file ends with .js, then load it. require will return a function for our route script, so just run it right away passing app to it.
-    if(files[i].match('\.js$')) require(baseDir + '/' + files[i])(app, conf, mongoose);
-  }
-})(app, conf, mongoose);
+loadModules('/routes', /\.js$/, app, conf, mongoose);
 
 app.listen(conf.port);
 util.log("Express server listening on port "+app.address().port+" in "+app.settings.env+" mode");
+
+/**
+  Convenience function to batch load a bunch of modules from a directory, and call the function with supplied arguments.
+  This only works for modules that are in the form of "module.exports = function(arg1){doStuff;}"
+  After specifying the arguments that are required, you may optionally pass additional arguments that should be given to the module.
+  @param baseDir - baseDirectory directory to load modules from (ie. "/routes")
+  @param fileRegEx - regular expression that matches the files to include (ie. /\.js$/ to grab any file that ends with ".js")
+*/
+function loadModules(baseDir, fileRegEx) {
+  //set our base directory for routes
+  var baseDirectory = __dirname + baseDir;
+
+  //read the contents. Using sync here because we need to get results before moving on with server setup.
+  var files = require('fs').readdirSync(baseDirectory);
+  for(var i in files) {
+    //if the file matches the regex, then load it. require will return a function.
+    //use the extra arguments given to this function to call the function
+    if(files[i].match(fileRegEx)) require(baseDirectory + '/' + files[i]).apply(this, _u.toArray(arguments).slice(2));
+  }
+}
 
